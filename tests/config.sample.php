30dfed8aaca62cb7d2293762006ccc975e5ff34f
slawek <slawomir.zytko@gmail.com>
2014-12-19 14:41:57 +0100

    Db fix

--- tests/config.sample.php
+++ tests/config.sample.php
@@ -8,7 +8,7 @@ return [
         "dbname" => "vegas_test",
         "port" => 3306,
         "username" => "root",
-        "password" => "root",
+        "password" => "",
         "options" => [
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
         ]
